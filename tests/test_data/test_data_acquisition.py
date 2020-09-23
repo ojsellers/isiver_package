4dc92853d65e3ff991cabfb14a99b252b441fc9a
ojsellers <62207394+ojsellers@users.noreply.github.com>
2020-09-23 09:49:54 +0100

    Update test_data_acquisition.py

--- tests/test_data/test_data_acquisition.py
+++ tests/test_data/test_data_acquisition.py
@@ -1,3 +1,11 @@
+'''
+unit tests for data_acquisition file and stock_dataframe class
+'''
+
+from isiver_utils.data.data_acquisition import stock_dataframe
+from datetime import datetime, timedelta, date
+import pandas as pd
+
 def test_data(ticker='SMT_L'):
     '''Fn to test stock_dataframe and the yfinance API is working
     - len(df) assertion is given range to account for bank holidays at the ends
