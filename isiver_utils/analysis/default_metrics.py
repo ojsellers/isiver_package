fad9588ed8fea14347cd11fb90b3967674c31926
Xoisai <isaacrayment123@gmail.com>
2020-09-25 17:19:13 +0100

    modified default_metrics

--- isiver_utils/analysis/default_metrics.py
+++ isiver_utils/analysis/default_metrics.py
@@ -13,7 +13,7 @@ def get_rolling_metric(df, column, metric, *windows):
     pass
 
 
-def moving_averages(df, column):
+def ma(df, *columns, windows=(20, 30, 50)):
     '''
     Generalised function to calculate and append a moving averages column
 
@@ -21,75 +21,85 @@ def moving_averages(df, column):
     :param windows: args int of time windows (days) for moving averages
             to be calculated over
     '''
-    df[f'{column}_MA_{w}'] = df[column].rolling(window=w).mean()
+    for c in columns:
+        for w in windows:
+            check_columns(F'{c}_MA_{w}')
+            df[f'{c}_MA_{w}'] = df[c].rolling(window=w).mean()
     return df
 
 
-def exp_moving_averages(df, column):
+def exp_ma(df, *columns, windows=(12, 26)):
     '''
-    Generalised function to calculate exponential moving averages
+    Generalised function to calculate exponential moving averages for given
+    column(s)
     '''
-    df[f'{column}_EMA_{w}'] = df['Close'].ewm(span=w).mean()
+    for c in columns:
+        for w in windows:
+            check_columns(F'{c}_EMA_{w}')
+            df[F'{c}_EMA_{w}'] = df[c].ewm(span=w).mean()
     return df
 
 
-def close_ma(df, windows=(20, 30, 50)):
-    '''
-    DEPRECATED
-    Function to calculate moving averages for close price.
-    '''
-    for w in windows:
-        check_columns(F'MA-{w}')
-        df[F'MA-{w}'] = df['Close'].rolling(window=w).mean()
-    return df
+# def close_ma(df, windows=(20, 30, 50)):
+#     '''
+#     DEPRECATED
+#     Function to calculate moving averages for close price.
+#     '''
+#     for w in windows:
+#         check_columns(F'MA_{w}')
+#         df[F'MA_{w}'] = df['Close'].rolling(window=w).mean()
+#     return df
+#
+#
+# def close_exp_ma(df, windows=(12, 26)):
+#     '''
+#     DEPRECATED
+#     Function to calculate exponential moving averages for close price.
+#     '''
+#     for w in windows:
+#         check_columns(F'EMA_{w}')
+#         df[F'EMA_{w}'] = df['Close'].ewm(span=w).mean()
+#     return df
 
 
-def close_exp_ma(df, windows=(12, 26)):
-    '''
-    DEPRECATED
-    Function to calculate exponential moving averages for close price.
-    '''
-    for w in windows:
-        check_columns(F'EMA-{w}')
-        df[F'EMA-{w}'] = df['Close'].ewm(span=w).mean()
-    return df
-
-
-def macd(df, windows=(12, 26)):
+def macd(df, *columns, windows=(12, 26)):
     '''
     Function to add moving average convergence divergence column to df.
     Default value uses 12 and 26 period ema's
     '''
-    check_columns(F'MACD-{windows[0]}-{windows[1]}')
-    if F'EMA-{windows[0]}' or F'EMA-{windows[1]}' not in df:
-        close_exp_ma(df, windows=(windows[0], windows[1]))
-    df[F'MACD-{windows[0]}-{windows[1]}'] = \
-        df[F'EMA-{windows[0]}'] - df[F'EMA-{windows[1]}']
+    for c in columns:
+        check_columns(F'{c}_MACD_{windows[0]}_{windows[1]}')
+        if F'{c}_EMA_{windows[0]}' or F'{c}_EMA_{windows[1]}' not in df:
+            exp_ma(df, c, windows=(windows[0], windows[1]))
+        df[F'{c}_MACD_{windows[0]}_{windows[1]}'] = \
+            df[F'{c}_EMA_{windows[0]}'] - df[F'{c}_EMA_{windows[1]}']
 
 
-def bollinger(df, windows=(20,)):
+def bollinger(df, *columns, windows=(20,)):
     '''
     Fn to calculate upper and lower bollinger bands for stock close price
     '''
-    for w in windows:
-        check_columns(F'BollUpper-{w}', F'BollLower-{w}')
-        if F'MA-{w}' or F'MASD-{w}' not in df:
-            close_ma(df, (w,))
-            ma_std(df, (w,))
-        df[F'BollUpper-{w}'] = \
-            df[F'MA-{w}'] + (2 * df[F'MASD-{w}'])
-        df[F'BollLower-{w}'] = \
-            df[F'MA-{w}'] - (2 * df[F'MASD-{w}'])
+    for c in columns:
+        for w in windows:
+            check_columns(F'{c}_Boll_Upper_{w}', F'{c}_Boll_Lower_{w}')
+            if F'{c}_MA_{w}' or F'{c}_MA_{w}_SD' not in df:
+                ma(df, 'Close', windows=(w,))
+                std(df, F'{c}_MA_{w}', windows=(w,))
+            df[F'{c}_Boll_Upper_{w}'] = \
+                df[F'{c}_MA_{w}'] + (2 * df[F'{c}_MA_{w}_SD'])
+            df[F'{c}_Boll_Lower_{w}'] = \
+                df[F'{c}_MA_{w}'] - (2 * df[F'{c}_MA_{w}_SD'])
 
 
-def ma_std(df, windows=(20,)):
+def std(df, *columns, windows=(20,)):
     '''
-    Fn to calculate standard deviation of close price for specified
-    rolling windows
+    Function to calculate standard deviation of given column(s), over specified
+    window.
     '''
-    for w in windows:
-        check_columns(df, F'MASD-{w}')
-        df[F'MASD-{w}'] = df['Close'].rolling(window=w).std()
+    for c in columns:
+        for w in windows:
+            check_columns(df, F'{c}_SD')
+            df[F'{c}_SD'] = df[c].rolling(window=w).std()
     return df
 
 
@@ -97,12 +107,11 @@ def get_def_metrics(df):
     '''
     Function to apply all above metrics to supplied stock dataframe.
     '''
-    print(df)
-    close_ma(df)
-    close_exp_ma(df)
-    macd(df)
-    ma_std(df)
-    bollinger(df)
+    ma(df, 'Close')
+    exp_ma(df, 'Close')
+    macd(df, 'Close')
+    bollinger(df, 'Close')
+    std(df, 'Close_MA_20')
     return df
 
 
