a0a7f3ae75b2d20f6ab3b37afe4ca32cb24345d5
Xoisai <isaacrayment123@gmail.com>
2020-09-28 14:56:12 +0100

    modified pickling function

--- isiver_utils/data/pickle_data.py
+++ isiver_utils/data/pickle_data.py
@@ -6,7 +6,7 @@ for ease of access.
 '''
 import pickle
 import pandas as pd
-import data_acquisition
+from isiver_utils.data import data_acquisition
 from datetime import date, timedelta
 
 
@@ -15,11 +15,9 @@ def pickle_stock_class(ticker, history_days, outdir):
     Function to download and preprocess a stock via data_acquisition, and pickle
     to given directory.
     '''
+    out_loc = f'{outdir}/{ticker}-h-{history_days}-{date.today()}.pkl'
     start_date = str(date.today() - timedelta(days=history_days))
     stock_class = data_acquisition.stock_dataframe(ticker, start_date, pd.DataFrame())
     stock_class.new_stock_df()
     stock_class.pre_process(True)
-    pickle.dump(stock_class, open(outdir+'example_data_class.pkl', 'wb'))
-
-
-pickle_stock_class('SMT_L', 200, 'examples/')
+    pickle.dump(stock_class, open(out_loc, 'wb'))
